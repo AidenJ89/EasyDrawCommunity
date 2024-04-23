205003bbc298d34a00b1b7b2029b6a1112398bd5
AidenJ89 <103896865+AidenJ89@users.noreply.github.com>
2024-04-23 17:06:22 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -5,8 +5,8 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: '欢迎来到易绘AI社区', url: 'https://easydraw.ac.cn' },
+    { title: '新人必看社区指南快速成长', url: 'https://easydraw.ac.cn/newmumber' }
   ],
 
   // 英雄区左右侧组件颠倒位置
@@ -15,12 +15,12 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
+  HEO_HERO_TITLE_1: '分享AI技术',
   HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_3: 'EASYDRAW.AC.CN',
+  HEO_HERO_TITLE_4: '新人必看',
+  HEO_HERO_TITLE_5: '社区指南助你快速成长',
+  HEO_HERO_TITLE_LINK: 'https://easydraw.ac.cn/newmumber',
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
@@ -36,10 +36,10 @@ const CONFIG = {
   HEO_INFOCARD_GREETINGS: [
     '你好！我是',
     '🔍 分享与热心帮助',
-    '🤝 专修交互与设计',
+    '🤝 专修AI领域技术',
     '🏃 脚踏实地行动派',
-    '🏠 智能家居小能手',
-    '🤖️ 数码科技爱好者',
+    '🏠 智能办公小能手',
+    '🤖️ 未来科技爱好者',
     '🧱 团队小组发动机'
   ],
   HEO_INFO_CARD_URL: 'https://easydrow.ac.cn', // 个人资料底部按钮链接
@@ -105,10 +105,10 @@ const CONFIG = {
   ],
 
   HEO_SOCIAL_CARD: true, // 是否显示右侧，点击加入社群按钮
-  HEO_SOCIAL_CARD_TITLE_1: '交流频道',
-  HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
+  HEO_SOCIAL_CARD_TITLE_1: '实操交流频道',
+  HEO_SOCIAL_CARD_TITLE_2: '加入我们社群共同实践成长',
   HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
-  HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
+  HEO_SOCIAL_CARD_URL: 'https://easydraw.ac.cn/article/2dd95a33-e175-43f8-9d43-6ca31ac32084',
 
   // *****  以下配置无效，只是预留开发 ****
   // 菜单配置
